45c7a53390ef5dd77dd45776542a9bf3d8bb2486
sebastians9517 <sebastians9517@gmail.com>
2021-01-06 08:37:07 -0600

    first commit

---
+++ src/pages/App/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React, { Component } from 'react';
+import { Route } from "react-router-dom";
+import NavBar from "../../components/NavBar/NavBar";
+
+class App extends Component {
+  state = {
+    navItems: [
+      {url: "/classlist", name: "Class List"},
+      {url: "/monsterlist", name: "Scary Monsters"},
+      {url: "/spellSearch", name: "Search for Spells"}
+    ]
+  }
+
+  render() {
+    return (
+      <>
+        <NavBar navItems={this.state.navItems}/>
+      </>
+    )
+  }
+}
+
+export default App;
